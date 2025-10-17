a83b747f58501494285b36866f46e421ec06ff4a
Ants Aas <ants.aas.v65@gmail.com>
2025-10-17 11:26:44 +0300

    17.10 tund

--- KooliProjekt.Application/Data/ApplicationDbContext.cs
+++ KooliProjekt.Application/Data/ApplicationDbContext.cs
@@ -7,13 +7,37 @@ using Microsoft.EntityFrameworkCore;
 
 namespace KooliProjekt.Application.Data
 {
+    public class Klient
+    {
+        public int id {  get; set; }
+        public string nimi { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+    }
+
+    public class Auto
+    {
+        public int id { get; set; }
+        public bool broneeritav { get; set; }
+        public string tüüp {  get; set; }
+    }
+
+    public class Arve
+    {
+        public int id { get; set; }
+        public int arve_omanik { get; set; }
+        public int summa {  get; set; }
+        public int rendi_aeg { get; set; }
+    }
+
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-
-        public DbSet<ToDoList> ToDoLists { get; set; }
-        public DbSet<ToDoItem> ToDoItems { get; set; }
+        public DbSet<Klient> to_klient {  get; set; }
+        public DbSet<Auto> to_auto { get; set; }
+        public DbSet<Arve> to_arve { get; set; }
     }
+
 }
---
+++ KooliProjekt.Application/Migrations/20251017075448_Initial.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KooliProjekt.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "to_arve",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    arve_omanik = table.Column<int>(type: "int", nullable: false),
+                    summa = table.Column<int>(type: "int", nullable: false),
+                    rendi_aeg = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_to_arve", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "to_auto",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    broneeritav = table.Column<bool>(type: "bit", nullable: false),
+                    tüüp = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_to_auto", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "to_klient",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nimi = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    phone = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_to_klient", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToDoLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoLists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToDoItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDone = table.Column<bool>(type: "bit", nullable: false),
+                    ToDoListId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ToDoItems_ToDoLists_ToDoListId",
+                        column: x => x.ToDoListId,
+                        principalTable: "ToDoLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDoItems_ToDoListId",
+                table: "ToDoItems",
+                column: "ToDoListId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "to_arve");
+
+            migrationBuilder.DropTable(
+                name: "to_auto");
+
+            migrationBuilder.DropTable(
+                name: "to_klient");
+
+            migrationBuilder.DropTable(
+                name: "ToDoItems");
+
+            migrationBuilder.DropTable(
+                name: "ToDoLists");
+        }
+    }
+}
