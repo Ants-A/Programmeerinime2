1aa1a9c07b8ce2f7c140dff1141d85fa02cfceef
Ants Aas <ants.aas.v65@gmail.com>
2025-12-05 09:19:05 +0200

    Maini data annotations fixed

--- KooliProjekt.Application/Data/Arve.cs
+++ KooliProjekt.Application/Data/Arve.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace KooliProjekt.Application.Data
     public class Arve
     {
         public int id { get; set; }
+        [Required]
         public int arve_omanik { get; set; }
+        [Required]
         public int summa { get; set; }
+        [Required]
         public int rendi_aeg { get; set; }
     }
 }
